be93c9531ea81857124f93666035697e07b6e3ab
patrykgolan <patrykgolan@gmail.com>
2023-03-31 16:21:25 +0100

    Remove DB connection form Controller.php

--- app/Controller/Controller.php
+++ app/Controller/Controller.php
@@ -2,32 +2,10 @@
 
 namespace App\Controller;
 
+use App\Database\DatabaseConnection;
+
 abstract class Controller
 {
-    protected $db;
-
-    protected function db()
-    {
-        if (!$this->db) {
-            try {
-                $this->db = new \PDO(
-                    sprintf(
-                        '%s:host=%s;port=%d;dbname=%s', 
-                        $_ENV['DB_TYPE'], 
-                        $_ENV['DB_HOST'], 
-                        $_ENV['DB_PORT'], 
-                        $_ENV['DB_NAME']
-                    ),
-                    $_ENV['DB_USER'],
-                    $_ENV['DB_PASSWORD']
-                );
-            } catch (\PDOException $e) {
-                throw $e;
-            }
-        }
-
-        return $this->db;
-    }
 
     /**
      * Renders a twig template with the params provided.
