413860af8a2041023c8cfbb1730b6717e2b2caaf
patrykgolan <patrykgolan@gmail.com>
2023-04-01 13:39:45 +0100

    CompanyMatchingSettings.php Database model added

---
+++ app/Models/CompanyMatchingSettings.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\DbModel;
+
+class CompanyMatchingSettings extends DbModel
+{
+    protected int $company_id;
+    protected array $postcodes;
+    protected array $bedrooms;
+    protected array $type;
+    public static function tableName(): string
+    {
+        return 'company_matching_settings';
+    }
+
+    public static function attributes(): array
+    {
+        return ['company_id', 'postcodes', 'bedrooms', 'type'];
+    }
+
+    public static function primaryKey(): string
+    {
+        return 'id';
+    }
+
+
+    public function rules(): array
+    {
+        return [];
+    }
+}
