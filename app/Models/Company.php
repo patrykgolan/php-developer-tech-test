1bd5f5d80a4e10c64e07ac821c9e14da3fa19035
patrykgolan <patrykgolan@gmail.com>
2023-04-01 14:57:40 +0100

    Company class added

---
+++ app/Models/Company.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+class Company extends \App\DbModel
+{
+
+    public static function tableName(): string
+    {
+        // TODO: Implement tableName() method.
+    }
+
+    public static function attributes(): array
+    {
+        // TODO: Implement attributes() method.
+    }
+
+    public static function primaryKey(): string
+    {
+        // TODO: Implement primaryKey() method.
+    }
+
+    public function rules(): array
+    {
+        // TODO: Implement rules() method.
+    }
+}
