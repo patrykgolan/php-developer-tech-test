01e7e4ba8dd34b560db27c8977ca434704676972
patrykgolan <patrykgolan@gmail.com>
2023-04-01 14:20:12 +0100

    match and pick methods changed

--- app/Service/CompanyMatcher.php
+++ app/Service/CompanyMatcher.php
@@ -2,45 +2,58 @@
 
 namespace App\Service;
 
-use App\DbModel;
+use App\Models\CompanyMatchingSettings;
 
-class CompanyMatcher extends DbModel
+class CompanyMatcher
 {
-
     private array $matches = [];
 
-    public function match()
+
+    public function match(string $postcode, int $bedrooms, string $types) : void
     {
-        
+        $where = [
+            [
+                'column' => 'postcode',
+                'operator' => 'LIKE',
+                'value' => '%'.$postcode.'%'
+            ],
+            [
+                'column' => 'bedrooms',
+                'operator' => 'LIKE',
+                'value' => '%'.$bedrooms.'%'
+            ],
+            [
+                'column' => 'types',
+                'operator' => '=',
+                'value' => $types
+            ],
+        ];
+
+        $this->matches =  CompanyMatchingSettings::findAllWhere($where);
     }
 
-    public function pick(int $count)
+    public function pick($count = null): void
     {
-        
+        // get default count
+        $count = $count ?? $_ENV['MAX_MATCHED_COMPANIES'];
+
+        // get current matches count
+        $currentCount = $count($this->matches);
+
+        // reduce matches if current count is higher the pick count
+        if($currentCount > $count){
+
+            // get random elements
+            $this->matches = array_rand($this->matches, $count);
+
+        }
+
     }
 
-    public function results(): array
-    {
-        return $this->matches;
-    }
 
     public function deductCredits()
     {
         
     }
 
-    public static function tableName(): string
-    {
-        // TODO: Implement tableName() method.
-    }
-
-    public static function attributes(): array
-    {
-        // TODO: Implement attributes() method.
-    }
-
-    public static function primaryKey(): string
-    {
-        // TODO: Implement primaryKey() method.
-    }
 }
