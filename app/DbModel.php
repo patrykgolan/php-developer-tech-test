f641b21516bd4ce14a82af5e0c6dc79da6bacb59
patrykgolan <patrykgolan@gmail.com>
2023-04-01 13:39:22 +0100

    findAllWhere changed to static

--- app/DbModel.php
+++ app/DbModel.php
@@ -29,7 +29,7 @@ namespace App;
      //         'operator' => '',
      //         'value' => '',
      //     ];
-     public function findAllWhere($where)
+     public  static function findAllWhere($where) : array
      {
          $tableName = static::tableName();
 
@@ -44,7 +44,7 @@ namespace App;
 
          $sql = implode(" AND ", $attributes);
 
-         $statement = $this->prepare("SELECT * FROM $tableName WHERE $sql");
+         $statement = self::prepare("SELECT * FROM $tableName WHERE $sql");
 
          foreach ($where as $condition){
              $column = $condition['column'];
