4ec679b52eeb577da5c0775f628cf720f7ca982d
patrykgolan <patrykgolan@gmail.com>
2023-04-01 15:01:49 +0100

    Company class stub

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,24 +4,31 @@ namespace App\Models;
 
 class Company extends \App\DbModel
 {
-
+    private int $id;
+    private  int $active;
+    private string $name;
+    private int $credits;
+    private  int $description;
+    private int $email;
+    private int $phone;
+    private int $website;
     public static function tableName(): string
     {
-        // TODO: Implement tableName() method.
+        return 'companies';
     }
 
     public static function attributes(): array
     {
-        // TODO: Implement attributes() method.
+        return ['id', 'active', 'name', 'credits', 'description', 'email', 'phone', 'website'];
     }
 
     public static function primaryKey(): string
     {
-        // TODO: Implement primaryKey() method.
+        return 'id';
     }
 
     public function rules(): array
     {
-        // TODO: Implement rules() method.
+        return [];
     }
 }
