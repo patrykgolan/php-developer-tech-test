2a67206e01cca93a09eb9b5f61f84254856b30e4
patrykgolan <patrykgolan@gmail.com>
2023-04-02 11:05:00 +0100

    added more/less to js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,44 +1,42 @@
-
-window.onload = () => {
+window.onload = (event) => {
     //
     //
     //-----SHOW MORE
     //
 
     // more 'link'
-    const moreButton = document.getElementsByClassName('matches__match__more')
+    const moreButton = document.querySelectorAll('.matches__match__more')
 
-    // add event listiner
-    for (let i = 0 ; i < moreButton.length; i++) {
-        moreButton[i].addEventListener('click', moreOnClick(), false)
+    // add event listener
+    for (let i = 0; i < moreButton.length; i++) {
+        moreButton[i].addEventListener('click', function (){
+            // get next sibling
+            const nextSibling = moreButton[i].nextElementSibling
+            // show more
+            if (nextSibling.classList.contains('match__match__details')) showMoreORless(nextSibling, moreButton[i])
+
+        });
     }
 
     // change display value
-    function showMore(container){
-        container.style.display = "flex";
+    function showMoreORless(container, button) {
+        if(container.style.display === 'none'){
+            container.style.display = "flex";
+            button.innerHTML = 'less'
+        } else {
+            container.style.display = "none";
+            button.innerHTML = 'more'
+        }
+
     }
 
     // onClick event for moreButton
-    function moreOnClick(){
-        let moreContainer;
-
+    function moreOnClick(index) {
         // get next sibling
-        let nextSibling = moreButton.nextElementSibling
+        const nextSibling = moreButton[index].nextElementSibling
+            // if next sibling is match__match__details show more
 
-        // loop through nodes and find next container with additional data
-        while (nextSibling) {
-            // if next sibling is match__match__details show more and break
-            if (nextSibling.classList.contains('match__match__details')){
-
-                moreContainer = nextSibling
-                showMore(moreContainer)
-                break
-            }
-
-            // go to another sibling if not
-            nextSibling = nextSibling.nextElementSibling
         }
-    }
 
     //
     //
@@ -46,38 +44,37 @@ window.onload = () => {
     //
     const submitButton = document.getElementById('submit-button')
 
-    function checkIfDisableCookieExists(){
+    function checkIfDisableCookieExists() {
         // get cookies
         const cookiesDecoded = decodeURIComponent(document.cookie);
         const cookiesArr = cookiesDecoded.split("; ");
 
-        cookiesArr.forEach(val =>{
-            if(val.indexOf('disable=')) return true
+        cookiesArr.forEach(val => {
+            if (val.indexOf('disable=')) return true
         })
 
         return false
     }
 
 
-    function setDisableButtonCookie(){
+    function setDisableButtonCookie() {
         // prepare 30 days expiry date
         const now = new Date();
         const time = now.getTime();
-        const expireTime = time + 1000*36000;
+        const expireTime = time + 1000 * 36000;
         now.setTime(expireTime);
 
-        document.cookie = 'disable=true;expires='+now.toUTCString()
+        document.cookie = 'disable=true;expires=' + now.toUTCString()
 
     }
 
     // set cookie if there results where shown
-    if(moreButton.length > 0 && checkIfDisableCookieExists()){
+    if (moreButton.length > 0 && checkIfDisableCookieExists()) {
         setDisableButtonCookie()
     }
 
     // disable button if cookie exists
-    if(submitButton && checkIfDisableCookieExists()){
+    if (submitButton && checkIfDisableCookieExists()) {
         submitButton.disabled = true
     }
-
 }
